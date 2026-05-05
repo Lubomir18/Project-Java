174f01bb5dca0f92f2269a188cd14caf3c1418f9
L <lubchokandarov@gmail.com>
2026-05-05 12:02:36 +0300

    test

--- src/SVGProject/SVG/SvgWriter.java
+++ src/SVGProject/SVG/SvgWriter.java
@@ -1,10 +1,29 @@
 package SVGProject.SVG;
 
+import SVGProject.Shape.ShapeBase;
 import SVGProject.Shape.ShapeRepository;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class SvgWriter {
 
     public void save(String path, ShapeRepository repo){
+
+        try (PrintWriter pw = new PrintWriter(new FileWriter(path))){
+
+            pw.println("<svg xmlns=\"http://www.w3.org/2000/svg\">");
+
+            for(ShapeBase s : repo.getAll()){
+                pw.println(" " + s.toSVG());
+            }
+
+            pw.println("/svg");
+
+        } catch (Exception e) {
+            System.out.println("Error writing SVG");;
+        }
 
     }
 
