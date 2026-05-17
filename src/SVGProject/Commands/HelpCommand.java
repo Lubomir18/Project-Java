13dd4a95ca69797331bf82a8ba0bc99ce411f33b
Lyubo <lubchokandarov@gmail.com>
2026-05-17 15:04:40 +0300

    test

--- src/SVGProject/Commands/HelpCommand.java
+++ src/SVGProject/Commands/HelpCommand.java
@@ -13,43 +13,35 @@ public class HelpCommand implements Command {
 
     @Override
     public void execute(String[] args) {
-        Console.log("Available Commands");
-        Console.log("Help - Shows all commands");
-        Console.log("Help Open - Info about Open Command");
-        Console.log("Help Close - Info about Close Command");
-        Console.log("Help Save - Info about Save Command");
-        Console.log("Help Save AS - Info about Save AS Command");
-        Console.log("Help Exit - Info about Exit Command");
-        Console.log("Open");
-        Console.log("Close");
-        Console.log("Save");
-        Console.log("Save AS");
-        Console.log("Exit");
+
+        if (args.length == 1) {
+            printAll();
+            return;
+        }
+
+        printCommand(args[1].toLowerCase());
 
     }
 
+    private void printAll() {
 
-    public static void CommandHelpCommands(String command){
+        Console.log("Available commands:");
 
-        if(command.equalsIgnoreCase("Open")) {
-            Console.log("Command Open. Makes it easier to open a specific file.");
-            Console.log("While opening a specific file you must be specific to its file location.");
-        }
-        else if(command.equalsIgnoreCase("Close")) {
-            Console.log("Command Close. Closes the File opened by the user without Saving it.");
-        }
-        else if(command.equalsIgnoreCase("Save")) {
-            Console.log("Command Save. Overrides the File in the same location and name.");
-        }
-        else if(command.equalsIgnoreCase("Save AS")) {
-            Console.log("Command Save AS. The User can Save the File to a new Directory.");
-        }
-        else if(command.equalsIgnoreCase("Exit")) {
-            Console.log("Command Exit. Exiting the Program without Saving.");
-        }
-        else{
-            Console.log("No Help found for this Command.");
-        }
+        Console.log("open <file>      - Open SVG file");
+        Console.log("close            - Close current file");
+        Console.log("save             - Save current file");
+        Console.log("saveas <file>    - Save file as new path");
+        Console.log("create circle ...- Create circle");
+        Console.log("create rectangle - Create rectangle");
+        Console.log("print            - Print all shapes");
+        Console.log("help [command]   - Show help");
+        Console.log("exit             - Exit program");
+    }
+
+
+    public static void printCommand(String command){
+
+
 
     }
 
