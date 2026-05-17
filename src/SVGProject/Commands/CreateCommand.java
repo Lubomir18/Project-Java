08ffc9454ccaedcbbda5c79426a23dcc0e7fc0e6
Lyubo <lubchokandarov@gmail.com>
2026-05-17 18:09:32 +0300

    fixed color when not set

--- src/SVGProject/Commands/CreateCommand.java
+++ src/SVGProject/Commands/CreateCommand.java
@@ -20,20 +20,44 @@ public class CreateCommand implements Command {
             ShapeBase shape;
 
             if(args[1].equalsIgnoreCase("circle")){
+
+                if (args.length < 5) {
+                    Console.log("Usage: create circle cx cy r [color]");
+                    return;
+                }
+
+                String fill = "black";
+
+                if(args.length >= 6){
+                    fill = args[5];
+                }
+
                 shape = new Circle(
                         Double.parseDouble(args[2]),
                         Double.parseDouble(args[3]),
                         Double.parseDouble(args[4]),
-                        args[5]
+                        fill
                 );
             }
             else if(args[1].equalsIgnoreCase("rectangle")){
+
+                if (args.length < 6) {
+                    Console.log("Usage: create rectangle x y width height [color]");
+                    return;
+                }
+
+                String fill = "black";
+
+                if(args.length >= 7){
+                    fill = args[6];
+                }
+
                 shape = new Rectangle(
                         Double.parseDouble(args[2]),
                         Double.parseDouble(args[3]),
                         Double.parseDouble(args[4]),
                         Double.parseDouble(args[5]),
-                        args[6]
+                        fill
                 );
             } else {
                 Console.log("Unknown shape: " + args[1]);
