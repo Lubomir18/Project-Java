efff99b1018fc9508c086d741d4db7505bfe3a05
Lyubo <lubchokandarov@gmail.com>
2026-05-17 17:03:27 +0300

    test

--- src/SVGProject/Commands/HelpCommand.java
+++ src/SVGProject/Commands/HelpCommand.java
@@ -31,9 +31,11 @@ public class HelpCommand implements Command {
         Console.log("close            - Close current file");
         Console.log("save             - Save current file");
         Console.log("saveas <file>    - Save file as new path");
-        Console.log("create circle ...- Create circle");
+        Console.log("create circle    - Create circle");
         Console.log("create rectangle - Create rectangle");
         Console.log("print            - Print all shapes");
+        Console.log("translate dx dy  - Moves all figures");
+        Console.log("translate n dx dy- Only moves n figure ");
         Console.log("help [command]   - Show help");
         Console.log("exit             - Exit program");
     }
@@ -41,7 +43,63 @@ public class HelpCommand implements Command {
 
     public static void printCommand(String command){
 
+        switch (command) {
 
+            case "open":
+                Console.log("[command] Open <file>");
+                Console.log("Opens SVG file and loads shapes.");
+                break;
+
+            case "close":
+                Console.log("[command] Close");
+                Console.log("Closes current opened file.");
+                break;
+
+            case "save":
+                Console.log("[command] Save");
+                Console.log("Saves current opened file.");
+                break;
+
+            case "saveas":
+                Console.log("[command] Saveas <file>");
+                Console.log("Saves file with new name.");
+                break;
+
+            case "create":
+                Console.log("[command] Create");
+                Console.log("Create circle cx cy r");
+                Console.log("Create rectangle x y width height");
+                break;
+
+            case "print":
+                Console.log("[command] Print");
+                Console.log("Prints all shapes.");
+                break;
+
+            case "erase":
+                Console.log("[command] Erase <n>");
+                Console.log("Deletes figure by number.");
+                break;
+
+            case "translate":
+                Console.log("Translate dx dy - Moves all figures");
+                Console.log("Translate n dx dy - Moves only n selected figure");
+                Console.log("To see what figure to select, use command print");
+                break;
+
+            case "help":
+                Console.log("[command] Help");
+                Console.log("Shows help information.");
+                break;
+
+            case "exit":
+                Console.log("[command] Exit");
+                Console.log("Exits application.");
+                break;
+
+            default:
+                Console.log("No help found for command: " + command);
+        }
 
     }
 
